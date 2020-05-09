3b99a790a831a0ff79f4ac4fc46aaad3ca79970e
Alex Girão <fco.alex.girao@gmail.com>
2020-05-09 01:33:51 -0300

    Configurando o prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // aspas simples
+  singleQuote: true,
+  // ultima virgula no final de objetos e arrays
+  trailingComma: 'all',
+  // evitar parenteses por volta de arrow functions que possuem um parametro
+  allowParens: 'avoud',
+};
