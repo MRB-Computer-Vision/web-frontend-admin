a307d5b66e1c759715fec8739dcc038f5fa78d35
Alex Girão <fco.alex.girao@gmail.com>
2020-05-24 15:32:39 -0300

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,10 @@ import Dashboard from '../pages/Dashboard';
 import Site from '../pages/Site';
 import Auth from '../pages/Auth';
 import SubmitExams from '../pages/SubmitExams';
-import ConsultExams from '../pages/ConsultExams';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SubmitExams} />
-    <Route path="/consult" exact component={ConsultExams} />
     <Route path="/submit" exact component={SubmitExams} />
     <Route path="/site" component={Site} />
     <Route path="/dashboard" component={Dashboard} />
