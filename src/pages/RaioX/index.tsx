2d206bbbdcfa1dc7b2fca4f378d2e1a297e905c2
Alex Maramaldo <alex@osbox.com.br>
2020-06-13 23:01:00 -0300

    adding tabs for the project

--- src/pages/RaioX/index.tsx
+++ src/pages/RaioX/index.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import AppBar from '../../components/AppBar';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,17 +46,45 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       padding: theme.spacing(5),
     },
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
   }),
 );
 
 const RaioX: React.FC = () => {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
   return (
     <>
       <AppBar titlePage="Raio-X" />
       <div className={classes.content}>
-        <h3>Raio-X</h3>
-        pagina de raio-x
+        <div className={classes.root}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="simple tabs example"
+          >
+            <Tab label="Item One" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <h3>Raio-X</h3>
+            pagina de raio-x
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Item Three
+          </TabPanel>
+        </div>
       </div>
     </>
   );
