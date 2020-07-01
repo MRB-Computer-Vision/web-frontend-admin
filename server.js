0c80e6209326c59c0b49f600541d8c8b5f75a1a7
Alex Maramaldo <alex@osbox.com.br>
2020-06-30 22:31:19 -0300

    changing port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
